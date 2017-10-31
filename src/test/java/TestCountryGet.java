b7b35f0cd57764ac42c709a62eb5bd329a770a86
Valera Tiurin <tiurinvalery@gmail.com>
2017-11-01 00:23:09 +0200

    Add travis

--- src/test/java/TestCountryGet.java
+++ src/test/java/TestCountryGet.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertTrue;
 public class TestCountryGet {
     @Test
     public void testCountryGet(){
-        Country executeCountry = new Country(1, " Ukraine");
+        Country executeCountry = new Country(1, "Ukraine");
         Country testCountry = Country.getCountry(1);
         assertTrue(executeCountry.equals(testCountry));
     }
